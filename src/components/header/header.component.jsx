7b0252a5833c60b84e46b2bd6b88119333bb2cc9
Surya Winata <s.winata@accenture.com>
2019-08-04 23:56:53 +1000

    add reduz

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./header.styles.scss";
+import { connect } from "react-redux";
+
+import { auth } from "../../firebase/firebase.utils";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-const Header = () => (
+
+import "./header.styles.scss";
+
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -11,11 +17,24 @@ const Header = () => (
       <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className="option" to="/contact">
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
